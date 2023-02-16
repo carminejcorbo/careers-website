a7263a8be5eb568c3c932ee4cb6be7d960500fd7
carmine corbo <56326227+carminejcorbo@users.noreply.github.com>
2023-02-16 19:08:08 +0000

    Created another template for job items. Add columns and buttons css.

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 
 app = Flask(__name__)
-
+JOBS =[
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Hartford, CT',
+    'salary': '$100,000.00'
+  },
+  {
+    'id': 2,
+    'title': 'Software Engineer',
+    'location': 'New Haven, CT',
+    'salary': '$125,000.00'
+  },
+  {
+    'id': 3,
+    'title': 'Backend Engineer',
+    'location': 'Remote',
+  },
+  {
+    'id': 4,
+    'title': 'Senior Help Desk',
+    'location': 'Waterbury,CT',
+    'salary': '$75,000.00'
+  }
+]
 @app.route('/')
 def helloWorld():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
+@app.route('/jobs')
+def list_job():
+  return jsonify(JOBS)
+  
 print(__name__)
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
